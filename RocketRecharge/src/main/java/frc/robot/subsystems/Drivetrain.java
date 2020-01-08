22cee70d89e832953ac07a2f18fc76f27207d6cc
nickynoodle16 <nicholas.howard@msmrockets.org>
2020-01-08 17:55:09 -0600

    drivetrain code

--- RocketRecharge/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ RocketRecharge/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,14 +6,34 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+//import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.DriverStation;
+//import edu.wpi.first.wpilibj.PIDController;
+//import edu.wpi.first.wpilibj.PIDOutput;
+
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class Drivetrain extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
+   
    */
-  public ExampleSubsystem() {
+
+   private Spark left1 = new Spark(0);
+   private Spark left2 = new Spark(1);
+   private Spark right1 = new Spark(2);
+   private Spark right2 = new Spark(3);
+   public double speed;
+
+  public Drivetrain() {
+
+    right2.follow(right1);
+    left2.follow(left1);   
 
   }
 

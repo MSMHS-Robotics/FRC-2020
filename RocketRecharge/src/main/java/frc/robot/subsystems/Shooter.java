4ecfdda84cdbc73a7617959b9871eb0702cbd186
bri4962 <bri@ousmiths.com>
2020-01-09 17:44:14 -0600

    Finished at the moment
    Finished fixing the shooter motor. All we need to add is constants and certain variables, which depend on what shooter we will be building.

--- RocketRecharge/src/main/java/frc/robot/subsystems/Shooter.java
+++ RocketRecharge/src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -20,11 +21,47 @@ public class Shooter extends SubsystemBase {
    * Creates a new ExampleSubsystem.
    */
   public Shooter() {
+
+    //angle motor config
     angleMotor.configAllowableClosedloopError(0, Constants.kPIDLoopIdx, Constants.kTimeoutMs);
+
+    /* Config the peak and nominal outputs */
+		angleMotor.configNominalOutputForward(0, Constants.kTimeoutMs);
+		angleMotor.configNominalOutputReverse(0, Constants.kTimeoutMs);
+		angleMotor.configPeakOutputForward(1, Constants.kTimeoutMs);
+		angleMotor.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+		/* Config the Velocity closed loop gains in slot0 */
+		angleMotor.config_kF(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kF, Constants.kTimeoutMs);
+		angleMotor.config_kP(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kP, Constants.kTimeoutMs);
+		angleMotor.config_kI(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kI, Constants.kTimeoutMs);
+    angleMotor.config_kD(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kD, Constants.kTimeoutMs);
+    
+    //shooter motor config
+
+      shooterMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,Constants.kPIDLoopIdx,Constants.kTimeoutMs);
+
+    /* Config the peak and nominal outputs */
+		shooterMotor.configNominalOutputForward(0, Constants.kTimeoutMs);
+		shooterMotor.configNominalOutputReverse(0, Constants.kTimeoutMs);
+		shooterMotor.configPeakOutputForward(1, Constants.kTimeoutMs);
+		shooterMotor.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+		/* Config the Velocity closed loop gains in slot0 */
+		shooterMotor.config_kF(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kF, Constants.kTimeoutMs);
+		shooterMotor.config_kP(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kP, Constants.kTimeoutMs);
+		shooterMotor.config_kI(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kI, Constants.kTimeoutMs);
+		shooterMotor.config_kD(Constants.kPIDLoopIdx, Constants.kGains_Velocit.kD, Constants.kTimeoutMs);
   }
 
   public boolean warmUp(double RPM) {
-    shooterMotor.pidWrite(RPM);
+    /**
+			 * Convert 500 RPM to units / 100ms.
+			 * 4096 Units/Rev * 500 RPM / 600 100ms/min in either direction:
+			 * velocity setpoint is in units/100ms
+			 */
+			double targetVelocity = RPM * 4096 / 600; 
+    shooterMotor.set(ControlMode.Velocity,targetVelocity);
     return true;
   }
 

8d5d5017c1c042a0d8b06dfd6090623807fa2e72
nickynoodle16 <nicholas.howard@msmrockets.org>
2020-01-09 17:37:43 -0600

    drivetrain possibly

--- RocketRecharge/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ RocketRecharge/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,16 +6,11 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-//import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SPI;
+
+//import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.DriverStation;
-//import edu.wpi.first.wpilibj.PIDController;
-//import edu.wpi.first.wpilibj.PIDOutput;
-
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
@@ -26,19 +21,30 @@ public class Drivetrain extends SubsystemBase {
 
    private Spark left1 = new Spark(0);
    private Spark left2 = new Spark(1);
-   private Spark right1 = new Spark(2);
-   private Spark right2 = new Spark(3);
+   private Spark left3 = new Spark( 2);
+   private Spark right1 = new Spark(3);
+   private Spark right2 = new Spark(4);
+   private Spark right3 = new Spark(5);
+   SpeedControllerGroup leftSide = new SpeedControllerGroup(left1, left2, left3);
+   SpeedControllerGroup rightSide = new SpeedControllerGroup(right1, right2, right3);
+   private DifferentialDrive drivetrain = new DifferentialDrive(leftSide, rightSide);
    public double speed;
+   //private double left;
+   
+   public Drivetrain() {
 
-  public Drivetrain() {
+     //Spark left1 = new Spark();
+     /*PIDOutput driveOutput = new PIDOutput(){
+      @Override
+      public void pidWrite(double output) {
+      */}
 
-    right2.follow(right1);
-    left2.follow(left1);   
-
-  }
-
-  @Override
-  public void periodic() {
+  /*@Override
+  public void teleopPeriodic() {
     // This method will be called once per scheduler run
+    drivetrain.tankDrive(, rightSpeed);
+  }*/
+  public void tankdrive(double left, double right ) {
+    drivetrain.tankDrive(Math.pow(-left, 3),Math.pow(-right, 3));
   }
 }

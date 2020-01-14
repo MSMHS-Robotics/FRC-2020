294c8aa280ef00cf03081b22c93f4d890e6e2971
bri4962 <bri@ousmiths.com>
2020-01-13 19:56:50 -0600

    tested drivetrain

--- RocketRecharge/src/main/java/frc/robot/Robot.java
+++ RocketRecharge/src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.RunDrivetrainCommand;
 import frc.robot.subsystems.Drivetrain;
 
 /**

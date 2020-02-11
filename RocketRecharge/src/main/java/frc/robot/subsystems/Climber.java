2df975ce1d5425cface4ca17e3e8923535bce673
nickynoodle16 <nicholas.howard@msmrockets.org>
2020-02-11 17:30:14 -0600

    Shuffleboard Thingz
    Hi, I like trains and programming stuff. I'm adding Shuffleboarding.
    
    XD

--- RocketRecharge/src/main/java/frc/robot/subsystems/Climber.java
+++ RocketRecharge/src/main/java/frc/robot/subsystems/Climber.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; //adding shuffleboard commands
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Climber extends SubsystemBase {
 
@@ -12,33 +16,40 @@ public class Climber extends SubsystemBase {
     // here. Call these from Commands.
 	//public CANSparkMax Climber;
 
-	private DigitalInput forwardLimitSwitch, reverseLimitSwitch;
-	private Talon climberMotor;
-	private Solenoid climberPistons;
-	private Boolean isDeployed = false;
-	
+	DigitalInput forwardLimitSwitch, reverseLimitSwitch;
+	Talon climberMotor;
+	Solenoid climberPistons;
+	Object arm;
+	private ShuffleboardTab Climbertab = Shuffleboard.getTab("Climber Tab");
+
+	private NetworkTableEntry CLIMBER_CLIMBER_SPEED = Climbertab.addPersistent("CLIMBER_CLIMBER_SPEED", Constants.CLIMBER_CLIMBER_SPEED).getEntry();
+	private NetworkTableEntry INTAKE_OUTTAKE_SPEED = Climbertab.addPersistent("INTAKE_OUTTAKE_SPEED", Constants.INTAKE_OUTTAKE_SPEED).getEntry();
+	private NetworkTableEntry motorposition = Climbertab.addPersistent("motorposition", Constants.motorPosition).getEntry();
+    private NetworkTableEntry motorUp = Climbertab.addPersistent("motorUp", Constants.motorUp).getEntry();
+
+    
+
+
 
 	public Climber() {
 		forwardLimitSwitch = new DigitalInput(1);
 		reverseLimitSwitch = new DigitalInput(2);
-		//need to assign actual channel values
+		//need to assign act2ual channel values
 		climberMotor = new Talon(5);
 		climberPistons = new Solenoid(7);
 	}
 
 	public void ClimberDeploy() {
 		climberPistons.set(true);
-		isDeployed = true;
 	}
 
 	public void raiseClimber() {
-		if(isDeployed) {
-			climberMotor.set(Constants.CLIMBER_CLIMBER_SPEED);
-		}
+		climberMotor.set(Constants.CLIMBER_CLIMBER_SPEED);
 	}
 
 	public void climbUp() {
 		climberMotor.set(-Constants.CLIMBER_CLIMBER_SPEED);
+		
 	}
 /*
 	public void ClimberStart() {
